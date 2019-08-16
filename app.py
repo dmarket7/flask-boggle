cfad8a8fa181ee57835cd98712cc07da91490102
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-08-16 12:11:12 -0700

    new folders

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from flask import Flask, request, render_template, redirect, flash, jsonify
+from unittest import TestCase
 from boggle import Boggle
 
+app = Flask(__name__)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = 'anything'
+
 boggle_game = Boggle()
+
+def display_board()
+
+@app.route('/')
+def create_game():
+    new_game = boggle_game.make_board()
+
+    return render_template('base.html', game_board=new_game)
+
+@app.route('/guess')
