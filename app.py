1fc63479922f26c5981c73c731ef64b3314d8b4c
Mac Glass <macglass@Mes-MacBook-Pro.localdomain>
2019-08-16 17:17:21 -0700

    Fridays over!

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = 'anything'
 
 BOARD_KEY = 'board'
-RESULTS_KEY = 'response'
+SCORE_KEY = 'score'
+HIGH_SCORE = 'high'
+GAMES_PLAYED = 'games'
 boggle_game = Boggle()
 
 
@@ -18,18 +20,43 @@ boggle_game = Boggle()
 def create_game():
     # new_game = boggle_game.make_board()
     session[BOARD_KEY] = boggle_game.make_board()
+    session[SCORE_KEY] = 0
 
+    if not session.get(HIGH_SCORE):
+        session[HIGH_SCORE] = session[SCORE_KEY]
+    
+    if session.get(GAMES_PLAYED):
+        games_played = session[GAMES_PLAYED]
+        session[GAMES_PLAYED] = games_played + 1 
+    else:
+        session[GAMES_PLAYED] = 1
+    
     return render_template('base.html', game_board=session[BOARD_KEY])
+
 
 @app.route('/guess', methods=["POST"])
 def check_guess():
     form_data = request.json['userGuess']
-    
     current_board = session[BOARD_KEY]
+    current_score = session[SCORE_KEY]
     print("current board: ", current_board)
     print("form data: ", form_data)
     guess_response = boggle_game.check_valid_word(current_board, form_data.lower())
 
-    print("Guess Response: ", guess_response)
+    high_score = session[HIGH_SCORE] 
+    print("RIGHT HERE!!!!!", session)
+    if guess_response == "ok":
+        session[SCORE_KEY] = len(form_data) + current_score
+        if session[SCORE_KEY] > high_score:
+            session[HIGH_SCORE] = session[SCORE_KEY]
     
-    return jsonify(response=guess_response)
+    return jsonify(response=guess_response, score=session[SCORE_KEY], highscore=session[HIGH_SCORE])
+
+
+@app.route('/finished')
+def checking_stats():
+    highest_score = session[HIGH_SCORE]
+    current_score = session[SCORE_KEY]
+    if current_score > highest_score:
+        session[HIGH_SCORE] = current_score
+    return jsonify(highscore=session[HIGH_SCORE])
--- static/boggle.js
+++ static/boggle.js
@@ -4,6 +4,9 @@ $(async function () {
   const $gameForm = $('#board');
   const $guess = $('#guess');
   const $guessResult = $('#guessResult')
+  const $scoreTracker = $('#scoreTracker')
+  const $submitButton = $('#submitButton')
+  const $highScoreTracker = $('#highScoreTracker')
 
   $gameForm.on("submit", async function(e){
     e.preventDefault();
@@ -11,9 +14,20 @@ $(async function () {
     let userGuess = $guess.val()
     console.log(userGuess)
     let checkValidWord = await axios.post(`/guess`, {userGuess})
-    console.log(checkValidWord)
+    console.log('here', checkValidWord.data.score)
     $guessResult.empty()
+    $scoreTracker.empty()
     $guessResult.append(checkValidWord.data.response)
+    $scoreTracker.append(checkValidWord.data.score)
+    $highScoreTracker.append(checkValidWord.data.highscore)
+
+    setTimeout(async function(){
+      $submitButton.remove()
+      $guess.remove()
+      let highestScore = await axios.get(`/finished`)
+      console.log(highestScore)
+    }, 5000);
   })
+
 
 });
